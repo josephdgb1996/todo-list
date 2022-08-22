4e434484385c73a1c899bba01d00f10c906ba490
Joseph Garcia <josephdgb1996@gmail.com>
2022-08-22 03:38:34 -0500

    settings

--- controllers/settings.js
+++ controllers/settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: process.env.PORT || 3000,
+    port: 3000,
     // Connect to MonggoDb, 
     // example atlas : mongodb+srv://zekaistodo:password@clusters.rqvdo.mongodb.net/Database?retryWrites=true&w=majority
     // example local : mongodb://127.0.0.1:27017
