eadde437b149ae12167832d492275c73d8f94a2e
Joseph Garcia <josephdgb1996@gmail.com>
2022-08-22 03:26:36 -0500

    changes

--- controllers/settings.js
+++ controllers/settings.js
@@ -3,5 +3,5 @@ module.exports = {
     // Connect to MonggoDb, 
     // example atlas : mongodb+srv://zekaistodo:password@clusters.rqvdo.mongodb.net/Database?retryWrites=true&w=majority
     // example local : mongodb://127.0.0.1:27017
-    dbURI: 'mongodb://54.173.65.125:27017,54.159.184.69:27017,34.226.153.225:27017/todo?replicaSet=rs0' 
+    dbURI: 'mongodb://54.173.65.125:27017,54.159.184.69:27017,34.226.153.225:27017/?replicaSet=rs0' 
 }
